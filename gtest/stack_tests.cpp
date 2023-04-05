88c4c4c5a7f42aaac041e5ad2c73c5d4552e0696
Mitchel Dickerson <mgdicker@uci.edu>
2023-04-05 12:37:42 -0700

    Changed heading.

--- gtest/stack_tests.cpp
+++ gtest/stack_tests.cpp
@@ -1,4 +1,4 @@
-// ------------------------- Tests File - knot_test.cpp --------------------- //
+// ------------------------- Tests File - stack_test.cpp -------------------- //
 // This file is for writing your own user tests. Be sure to include your *.h
 // files to be able to access the functions that you wrote for unit testing.
 // An example has been provided, but more documentation is available here:

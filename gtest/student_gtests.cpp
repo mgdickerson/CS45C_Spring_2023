a2bdeeb34348de003ebeede8a00ef27ae9ecf841
Mitchel Dickerson <mgdicker@uci.edu>
2023-04-21 12:55:32 -0700

    Shifted to new GTest evaluation.

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -2,5 +2,47 @@
 
 #include "string.hpp"
 
-// ADD YOUR TESTS HERE:
+TEST(StringFunction, strlen) {
+    EXPECT_EQ(String::strlen(""), 0);
+    EXPECT_EQ(String::strlen("foo"), 3);
+}
 
+TEST(StringFunction, strcpy) {
+    char result[10];
+    EXPECT_EQ(strcpy(result, "foo"), result);
+    EXPECT_STREQ(result, "foo");
+
+    EXPECT_EQ(strcpy(result, "a"), result);
+    EXPECT_STREQ(result, "a");
+
+    EXPECT_EQ(strcpy(result, ""), result);
+    EXPECT_STREQ(result, "");
+}
+
+TEST(StringFunction, strncpy) {
+    EXPECT_TRUE(false);
+}
+
+TEST(StringFunction, strcat) {
+    EXPECT_TRUE(false);
+}
+
+TEST(StringFunction, strncat) {
+    EXPECT_TRUE(false);
+}
+
+TEST(StringFunction, strcmp) {
+    EXPECT_TRUE(false);
+}
+
+TEST(StringFunction, reverse_cpy) {
+    EXPECT_TRUE(false);
+}
+
+TEST(StringFunction, strchr) {
+    EXPECT_TRUE(false);
+}
+
+TEST(StringFunction, strstr) {
+    EXPECT_TRUE(false);
+}

586c00367fc0b875049a09da162e749eb457a258
Mitchel Dickerson <mgdicker@uci.edu>
2023-05-20 09:08:14 -0700

    Fixed matrix ordering test.

--- gtest/matrix_gtests.cpp
+++ gtest/matrix_gtests.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 TEST(MatrixTests, RowsAndCols) {
     Matrix<int> mat{3, 2};
-    EXPECT_EQ(mat.num_rows(), 2);
-    EXPECT_EQ(mat.num_cols(), 3);
+    EXPECT_EQ(mat.num_rows(), 3);
+    EXPECT_EQ(mat.num_cols(), 2);
 
     Matrix<double> mat2{5, 1};
     EXPECT_EQ(mat.num_rows(), 5);
